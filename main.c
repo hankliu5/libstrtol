5c49513236bce4163f53c9ac30b46cd2748559ba
hankliu5 <yliu719@ucr.edu>
2019-07-02 16:03:38 -0700

    add timer function for reading and deserializing parts.

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main(int argc, char** argv) {
     int row, col, i, j;
     size_t data_len = 16;
     int *strtol_matrix = NULL;
-    uint64_t strtol_transform_time = 0;
+    uint64_t read_time = 0, strtol_transform_time = 0;
     struct timeval start_time, end_time;
 
     if (argc < 2) {
@@ -20,23 +20,29 @@ int main(int argc, char** argv) {
         return 1;
     }
 
+    gettimeofday(&start_time, NULL);
     printf("reading: %s\n", argv[1]);
     fd = open(argv[1], O_RDONLY);
     fstat(fd, &sb);
     start = calloc(sb.st_size, sizeof(char));
     read(fd, start, sb.st_size);
+    gettimeofday(&end_time, NULL);
+    read_time = ((end_time.tv_sec * 1000000 + end_time.tv_usec) - (start_time.tv_sec * 1000000 + start_time.tv_usec));
+
     
     gettimeofday(&start_time, NULL);
     strtol_matrix = int_deserialize(start, &row, &col);
     gettimeofday(&end_time, NULL);
-    strtol_transform_time += ((end_time.tv_sec * 1000000 + end_time.tv_usec) - (start_time.tv_sec * 1000000 + start_time.tv_usec));
+    strtol_transform_time = ((end_time.tv_sec * 1000000 + end_time.tv_usec) - (start_time.tv_sec * 1000000 + start_time.tv_usec));
     
     if (strtol_matrix == NULL) {
         return 1;
     }
 
     printf("row: %d, col: %d\n", row, col);
+    printf("read time: %lu usec\n", read_time);
     printf("strtol transform time: %lu usec\n", strtol_transform_time);
+    printf("total time: %lu usec\n", read_time+strtol_transform_time);
     
     free(strtol_matrix);
     free(start);
--- test.py
+++ test.py
@@ -10,29 +10,53 @@ if len(sys.argv) < 2:
     print("usage {} [input data]".format(sys.argv[0]))
     exit(1)
 
+read_time = 0
+strtol_transform_time = 0
+
 start = time.time()
 f = open(sys.argv[1], 'r')
 s = f.read()
-a = hello.deserialize(s)
-f.close()
 end = time.time()
-print("c api elapsed time: {}".format(end-start))
+read_time = end - start
+
+start = time.time()
+a = hello.deserialize(s)
+end = time.time()
+strtol_transform_time = end - start
+f.close()
+print("read time: {}".format(read_time))
+print("c api elapsed time: {}".format(strtol_transform_time))
 
 start = time.time()
 f = open(sys.argv[1], 'rb')
 s = f.read()
-b = cython_wrapper.cython_deserialize(s)
-f.close()
 end = time.time()
-print("cython elapsed time: {}".format(end-start))
+read_time = end - start
+
+start = time.time()
+b = cython_wrapper.cython_deserialize(s)
+end = time.time()
+strtol_transform_time = end - start
+
+f.close()
+print("read time: {}".format(read_time))
+print("cython elapsed time: {}".format(strtol_transform_time))
 
 start = time.time()
 f = open(sys.argv[1], 'r')
 s = f.readline().rstrip()
 row, _ = s.split()
-c = pandas.read_csv(f, sep=' ', header=None, nrows=int(row)).to_numpy()
-f.close()
 end = time.time()
-print("pandas elapsed time: {}".format(end-start))
+read_time = end - start
+
+start = time.time()
+c = pandas.read_csv(f, sep=' ', header=None, nrows=int(row)).to_numpy()
+end = time.time()
+strtol_transform_time = end - start
+
+f.close()
+
+print("read time: {}".format(read_time))
+print("pandas elapsed time: {}".format(strtol_transform_time))
 
 print('same: {}'.format(numpy.array_equal(a, b) and numpy.array_equal(a, c)))
